1545a0c1ebddfe38a6309aa5930f14d0145d49ba
Luck <git@lucko.me>
2021-04-07 20:22:34 +0100

    update homepage

--- pages/index.js
+++ pages/index.js
@@ -15,9 +15,9 @@ const AboutMe = () => {
   return (
     <article>
       <h1>About me</h1>
-      <p>I use the nickname "lucko" or sometimes just "Luck" for most of my online profiles and accounts.</p>
-      <p>Among other things, I'm a software developer and maintain/contribute to a number of open
-        source projects in my spare time.</p>
+      <p>I use the nickname "<b>lucko</b>" or sometimes just "<b>Luck</b>" for most of my online profiles and accounts.</p>
+      <p>Among other things, I'm a <b>software developer</b> and maintain/contribute to a number of <b>open
+        source</b> projects in my spare time.</p>
       <p>If you'd like to contact me, you can reach out via <a href="mailto:hey@lucko.me">email</a>{' '}
         or <a href="https://discord.gg/Rnbpc7s">Discord</a>.</p>
       <p className="message">I try my best to reply to all messages, but unfortunately I often
@@ -39,8 +39,8 @@ const Domain = () => {
       <ul>
         <li><a href="https://ci.lucko.me/">Jenkins</a> (continuous integration & downloads)</li>
         <li><a href="https://nexus.lucko.me/">Nexus</a> (maven repository)</li>
-        <li><a href="https://paste.lucko.me/">Paste</a> (pastebin)</li>
-        <li><a href="https://bytebin.lucko.me/">Bytebin</a> (content storage web service, see below for details)</li>
+        <li><a href="https://paste.lucko.me/">Paste</a> (pastebin, see below)</li>
+        <li><a href="https://bytebin.lucko.me/">Bytebin</a> (content storage web service, see below)</li>
       </ul>
       <p>I try my best to keep these services available. Most have been online and had good uptime
         for the past ~3 years at least.</p>
@@ -52,59 +52,90 @@ const Projects = () => {
   return (
     <article>
       <h1>My projects</h1>
-      <p>An abridged list of my publicly available projects.</p>
-      <h3><a href="https://github.com/lucko/LuckPerms">lucko/LuckPerms</a></h3>
-      <ul>
-        <li>LuckPerms is a permission plugin for Minecraft servers, written in Java.</li>
+      <p>I work on a number of open source projects in my spare time.</p>
+
+      <Project repo="lucko/LuckPerms">
+        <li>LuckPerms is a <b>permission</b> plugin for Minecraft servers, written in Java.</li>
         <li>At time of writing, it's been downloaded ~1.5M times, has ~250,000 monthly active users,
           and a Discord community of ~12,000 people.</li>
-        <li>The project also has a website and a number of web apps written using Vue.js.</li>
+        <li>The project also has a website, <a href="https://github.com/lucko/LuckPermsWeb">LuckPermsWeb</a>,
+          which includes web apps for viewing/editing/managing permission setups, written using Vue.js.</li>
         <li>More information can be found on the project homepage at <a href="https://luckperms.net/">luckperms.net</a>.</li>
-      </ul>
-      <h3><a href="https://github.com/lucko/spark">lucko/spark</a></h3>
-      <ul>
-        <li>spark is a performance profiling plugin/mod for Minecraft clients, servers and proxies,
+      </Project>
+
+      <Project repo="lucko/spark">
+        <li>spark is a <b>performance profiling</b> plugin/mod for Minecraft clients, servers and proxies,
           written in Java.</li>
-        <li>There is also a web viewer component, <a href="https://github.com/lucko/spark-viewer">spark-viewer</a>,
-          written using React.</li>
+        <li>The project also has a website, <a href="https://github.com/lucko/spark-viewer">spark-viewer</a>,
+          which includes functionality for inspecting profiles as stack trees and flame graphs, written using React.</li>
         <li>More information can be found on the project homepage at <a href="https://spark.lucko.me/">spark.lucko.me</a>.</li>
-      </ul>
-      <h3><a href="https://github.com/lucko/helper">lucko/helper</a></h3>
-      <ul>
-        <li>helper is a Bukkit plugin development toolkit/library. It provides a number of utilities
-          and extended APIs to support rapid and easy development of plugins.</li>
-        <li>I'm not currently working on further API additions, but the project is still being used
+      </Project>
+
+      <Project repo="lucko/helper">
+        <li>helper is a <b>development toolkit / library</b> for Bukkit plugin developers. It provides a number of utilities
+          and extended APIs which help to reduce boilerplate code and offer alternative paradigms for creating content.</li>
+        <li>I'm no longer actively working on further API additions, but the project is still being used
           frequntly by various development teams, and I accept pull requests!</li>
-      </ul>
-      <h3><a href="https://github.com/lucko/bytebin">lucko/bytebin</a></h3>
-      <ul>
-        <li>bytebin is a fast & lightweight content storage web service.</li>
-        <li>You can think of it a bit like a <a href="https://en.wikipedia.org/wiki/Pastebin">pastebin</a>,
-          except that it accepts any kind of data (not just plain text!)</li>
-      </ul>
-      <h3><a href="https://github.com/lucko/BungeeGuard">lucko/BungeeGuard</a></h3>
-      <ul>
-        <li>BungeeGuard is a plugin-based security/firewall solution for BungeeCord and Velocity proxies.</li>
-      </ul>
-      <h3>Other</h3>
-      <p>Below are some other noteworthy projects I've created or made significant contributions to.</p>
-      <ul>
-        <li><a href="https://github.com/lucko/shadow">lucko/shadow</a> (annotation based Java reflection library)</li>
-        <li><a href="https://github.com/lucko/jar-relocator">lucko/jar-relocator</a> (program to relocate packages within .jar files)</li>
-        <li><a href="https://github.com/lucko/commodore">lucko/commodore</a> (brigadier API for Bukkit)</li>
-        <li><a href="https://github.com/lucko/fabric-permissions-api">lucko/fabric-permissions-api</a> (simple permissions API for Fabric)</li>
-        <li><a href="https://github.com/lucko/ScriptController">lucko/ScriptController</a> (javax.script wrapper API)</li>
-        <li><a href="https://github.com/lucko/tapir">lucko/tapir</a> (JavaScript plugin loader for Sponge)</li>
-        <li><a href="https://github.com/lucko/synapse">lucko/synapse</a> (WIP Bukkit permissions API)</li>
-        <li><a href="https://github.com/lucko/lucko.github.io">lucko/lucko.github.io</a> (personal website, written using Next.js)</li>
-        <li><a href="https://github.com/KyoriPowered/adventure">KyoriPowered/adventure</a> (user-interface library for Minecraft plugins/mods)</li>
-        <li><a href="https://github.com/KyoriPowered/event">KyoriPowered/event</a> (Java event bus library)</li>
-        <li><a href="https://github.com/KyoriPowered/blossom">KyoriPowered/blossom</a> (Gradle plugin for source code tokens)</li>
-        <li><a href="https://github.com/SpongePowered/Configurate">SpongePowered/Configurate</a> (Java configuration library)</li>
-        <li>... and more! You can find all of my public work on my <a href="https://github.com/lucko">GitHub profile</a>.</li>
-      </ul>
+      </Project>
+
+      <Project repo="lucko/bytebin">
+        <li>bytebin is a fast & lightweight <b>content storage</b> web service, written in Java.</li>
+        <li>You can think of it a bit like a pastebin, except that it accepts any kind of
+          data (not just plain text!)</li>
+        <li>It has a straightforward HTTP API that supports CORS.</li>
+      </Project>
+
+      <Project repo="lucko/paste">
+        <li>paste (my own attempt at a <b>pastebin</b>) is a simple, "code friendly" web frontend for bytebin.</li>
+        <li>It is written using React, react-simple-code-editor and Prism.</li>
+      </Project>
+
+      <Project repo="lucko/BungeeGuard">
+        <li>BungeeGuard is a plugin-based <b>security/firewall</b> solution for BungeeCord and Velocity proxies.</li>
+      </Project>
+      
+      <section>
+        <h3>Other</h3>
+        <p>I've also created and made significant contributions to a number of other projects.</p>
+        <ul>
+          <BriefProject repo="lucko/shadow" desc="annotation based Java reflection library" />
+          <BriefProject repo="lucko/jar-relocator" desc="program to relocate packages within .jar files" />
+          <BriefProject repo="lucko/commodore" desc="brigadier API for Bukkit" />
+          <BriefProject repo="lucko/fabric-permissions-api" desc="simple permissions API for Fabric" />
+          <BriefProject repo="lucko/ScriptController" desc="javax.script wrapper API" />
+          <BriefProject repo="lucko/tapir" desc="JavaScript plugin loader for Sponge" />
+          <BriefProject repo="lucko/synapse" desc="WIP Bukkit permissions API" />
+          <BriefProject repo="lucko/lucko.github.io" desc="personal website, written using Next.js" />
+          <BriefProject repo="KyoriPowered/adventure" desc="user-interface library for Minecraft plugins/mods" />
+          <BriefProject repo="KyoriPowered/event" desc="Java event bus library" />
+          <BriefProject repo="KyoriPowered/blossom" desc="Gradle plugin for replacing tokens in source code" />
+          <BriefProject repo="SpongePowered/Configurate" desc="Java configuration library" />
+          <li>... and more! You can find all of my public repositories on my <a href="https://github.com/lucko">GitHub profile</a>.</li>
+        </ul>
+        <p>I'm a member of a couple of organisations.</p>
+        <ul>
+          <li><a href="https://kyori.net/">Kyori</a> - I've been primarily working on adventure lately (listed above)</li>
+          <li><a href="https://www.spongepowered.org/">Sponge</a> - I wrote parts of / maintain the Permissions Service in
+            SpongeAPI, and was previously a maintainer of Configurate (listed above).</li>
+        </ul>
+      </section>
     </article>
   )
+}
+
+const Project = ({ repo, children }) => {
+  return (
+    <section>
+      <h3><a href={'https://github.com/' + repo}>{repo}</a></h3>
+      <ul>
+        {children}
+      </ul>
+    </section>
+  )
+}
+
+const BriefProject = ({ repo, desc }) => {
+  return <li><a href={'https://github.com/' + repo}>{repo}</a> ({desc})</li>;
 }
 
 export const config = { amp: 'hybrid' }
